be7917646d3005f638a5a96beac1ab31b7092278
J.D. Lowe <jdlowe.dev@gmail.com>
2021-03-15 15:51:26 -0400

    Folder Restructure

--- core-game/src/game/Player.js
+++ core-game/src/game/Player.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 export default class Player extends React.Component {
     render() {
+        const isMe = this.props.isMe;
+        const nickname = this.props.nickname;
+        const isMod = this.props.isMod;
+        const amIMod = this.props.amIMod;
         return (
-            <div style={{backgroundColor: this.props.isMe ? "yellow" : ""}}>{this.props.nickname}<br />Mod: {this.props.isMod ? "Yes" : "No"}<br /> Am I mod? {this.props.amIMod ? "Yes" : "No"}</div>
+            <div style={{backgroundColor: isMe ? "yellow" : ""}}>{nickname}<br />Mod: {isMod ? "Yes" : "No"}<br /> Am I mod? {amIMod ? "Yes" : "No"}</div>
         )
     }
 }
