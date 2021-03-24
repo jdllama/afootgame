7713543d8eb8b258919303d4a8d497d8405e6e63
J.D. Lowe <jdlowe.dev@gmail.com>
2021-03-23 22:30:06 -0400

    More styling!

--- client/src/game/PlayerList.js
+++ client/src/game/PlayerList.js
@@ -4,14 +4,33 @@ import {ReactComponent as Logo} from "../hamburger.svg";
 import "./PlayerList.css";
 
 export default class PlayerList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            playersVisible: false,
+        };
+
+        this.togglePlayers = this.togglePlayers.bind(this);
+    }
+
+    togglePlayers() {
+        let playersVisible = !this.state.playersVisible;
+        this.setState({playersVisible})
+    }
+
     render() {
         let players = this.props.players;
-        let renders = players.map(player => {
-            return (<Player socketID={player.socketID} isThisPlayerMod={this.props.isThisPlayerMod} setThief={this.props.setThief} nickname={player.nickname} isMe={player.isMe} isMod={player.isMod} amIMod={player.isMe === true && player.isMod === true}/>);
-        })
+        let renders;
+        if(this.state.playersVisible === true) {
+            renders = players.map(player => {
+                return (<Player socketID={player.socketID} isThisPlayerMod={this.props.isThisPlayerMod} setThief={this.props.setThief} nickname={player.nickname} isMe={player.isMe} isMod={player.isMod} amIMod={player.isMe === true && player.isMod === true}/>);
+            })
+        }
+        
+
         return (
             <>
-            <Logo className="hamburger-icon" />
+                <button className="hamburger-button" onClick={this.togglePlayers}><Logo className="hamburger-icon" /></button>
             <div className="player-list">
                 {renders}
             </div>
